cb2f33a62e3c20e9beacdb21269f87480f4ddf68
JIbald <j.ibald@protonmail.ch>
2022-07-20 21:27:38 +0200

    fixes tcp_server, moves *.gpg to .gitignore

--- 02_BASIC_NETWORKING_TOOLS/tcp_server.py
+++ 02_BASIC_NETWORKING_TOOLS/tcp_server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 IP = '0.0.0.0'
-PORT = '9998'
+PORT = 9998
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +19,7 @@ def main():
 def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(1024)
-        print(f'[*] Received: {request.deode("utf-8")}')
+        print(f'[*] Received: {request.decode("utf-8")}')
         sock.send(b'ACK')
 
 if __name__ == '__main__':
--- 02_BASIC_NETWORKING_TOOLS/udp_client.py
+++ 02_BASIC_NETWORKING_TOOLS/udp_client.py
@@ -7,7 +7,7 @@ target_port = 9997
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
-client.sendto(b"AAABBBCCC", (target_host, target_port))
+client.sendto(b"just some test data", (target_host, target_port))
 
 # receive some data
 data, addr = client.recvfrom(4096)
